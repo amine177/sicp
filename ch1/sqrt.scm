23f78ebcd73e7d795b94f5b9129ce577c648de8e
aminos <amriamine@protonmail.com>
2020-02-13 17:12:05 +0100

    .

--- ch1/sqrt.scm
+++ ch1/sqrt.scm
@@ -1,7 +1,7 @@
 (define (refine lower upper)
-  (/ (+ lower (/ upper lower)) 2)) ; newton's method x_n+1 = x_n - f(x_n) / f'(x_n)
+  (/ (+ lower (/ upper lower)) 2.0)) ; newton's method x_n+1 = x_n - f(x_n) / f'(x_n)
 
-(define epsilon 0.00001) ; approximation value
+
 
 (define (square a) (* a a))
 
@@ -9,15 +9,21 @@
   (if (< a 0) (- a)
       a))
 
-(define (good y x)
-  (if (< (abs (- x (square y))) epsilon) true
+(define (good y x approx)
+  (if (< (abs (- x (square y))) approx) true
       false))
 
-(define (sqrt-next x_n x)
-  (if (good x_n x) ; predicate
+(define (sqrt-next x_n x approx)
+  (if (good x_n x approx) ; predicate
       x_n          ; consequent
-      (sqrt-next (refine x_n x) x)))
+      (sqrt-next (refine x_n x) x approx)))
 
+(define x_0 1)
 
+(define (sqrt x approx)
+  (round-by (sqrt-next x_0 x approx)) approx)
 
+(define epsilon 0.00001) ; approximation value
+(sqrt 2 epsilon)
+(sqrt 9 epsilon)
       
