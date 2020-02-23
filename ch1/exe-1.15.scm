6be0c41b0cab47bb91d879fe69753efadc4afbad
aminos <amriamine@protonmail.com>
2020-02-23 18:19:12 +0100

    fiftinth exercise

---
+++ ch1/exe-1.15.scm
@@ -0,0 +1,11 @@
+(define (cube x) (* x x x))
+(define (p x) (- (* 3 x) (* 4 (cude x))))
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+
+
+
+; x / 3^n = 10^-1 => n = Floor((log(x) + log(10))/log(3))+ 1 , for x = 12, n = 5
+; O(log(x)) for both space and time
