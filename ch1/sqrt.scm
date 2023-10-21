39f316416566cddd7b7449948f7ee79abb7cac2f
amine177 <amri.amine@protonmail.com>
2023-10-21 16:41:24 +0200

    Added more comments

--- ch1/sqrt.scm
+++ ch1/sqrt.scm
@@ -1,29 +1,19 @@
-(define (refine lower upper)
-  (/ (+ lower (/ upper lower)) 2.0)) ; newton's method x_n+1 = x_n - f(x_n) / f'(x_n)
+
+      
 
 
+(define (calculate_next x_n x)		; x_n+1 = x_n - f(x_n) / f'(x_n)
+					; f(x) = x^2 - y , solve for f(x) = 0
+ 					;   x_n+1 = x_n - (x_n^2 - 2) / 2*(x_n)
 
-(define (square a) (* a a))
-
+  (- x_n (/ (- (* x_n x_n) 2) (* 2 x_n))))
 (define (abs a)
   (if (< a 0) (- a)
       a))
-
-(define (good y x approx)
-  (if (< (abs (- x (square y))) approx) true
-      false))
-
-(define (sqrt-next x_n x approx)
-  (if (good x_n x approx) ; predicate
-      x_n          ; consequent
-      (sqrt-next (refine x_n x) x approx)))
-
-(define x_0 1)
-
-(define (sqrt x approx)
-  (round-by (sqrt-next x_0 x approx)) approx)
-
-(define epsilon 0.00001) ; approximation value
-(sqrt 2 epsilon)
-(sqrt 9 epsilon)
-      
+(define (sqrt-iter x x_n epsilon)
+  (if (<  (abs (-  (* x_n x_n) x))  epsilon)
+      x_n
+      (sqrt-iter x (calculate_next x_n x) epsilon)))
+(define (sqrt x  x_0 epsilon)
+  (sqrt-iter x x_0 epsilon))
+(sqrt 2 1 0.00000000000001)
