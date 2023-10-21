07b5e6f9ad0329b77444f3baa084f24929ee3feb
amine177 <amri.amine@protonmail.com>
2023-10-21 17:30:35 +0200

    Redone the exercise

--- ch1/exe-1.9.scm
+++ ch1/exe-1.9.scm
@@ -1,42 +1,56 @@
-(define (+ a b)
-  (if (= a 0)
-      b
-      (inc (+ (dec a) b))))
- ; the process of evaluation
- ; (+ 4 5)
- ; (inc (+ 3 5)
- ; (inc (inc (+ 2 5)))
- ; (inc (inc (inc (+ 1 5))))
- ; (inc (inc (inc (inc (+ 0 5)))))
- ; (inc (inc (inc (inc 5))))
- ; (inc (inc (inc 6)))
- ; (inc (inc 7))
- ; (inc 8)
- ; 9
- 
- 
-; this is a recursive process, because the evaluation
-; takes the form of a chain of deferred calls to (inc )
-; which needs the last call to pass its returned value to the previous call
-; in order to complete the computation. 
+
+(define (inc x)
+  (+ x 1))
+(define (dec x)
+  (- x 1))
+(define (sum a b)			; redefining + later will
+					;result in an infinte call
+  (if (= a 0) b				; chain so instead of
+      (inc (sum (dec a)  b))))		; redefining + like the book
+					;suggests we call it sum
 
 
-(define (+ a b)
-  (if (= a 0)
-      b
-      (+ (dec a) (inc b))))
- ; the process of evaluation
- ; (+ 3 6)
- ; (+ 2 7)
- ; (+ 1 8)
- ; (+ 0 9)
- ; 9
-; this is an iterative process because every new call to (+ ) doesn't need
-; an ever growing chain calls in order to have a correct state
-; state in this case is sufficiently described by the fixed amount of parameters
-; passe to (+ ) on every call
+(sum 4 5)				; (sum 4 5)
+					; (inc (sum (dec 4) 5))
+					; (inc (sum 3 5))
+					; (inc (inc (sum (dec 3) 5))
+					; (inc (inc (sum 2 5)))
+					; (inc (inc (inc (sum (dec 2)
+					; 5))))
+					; (inc (inc (inc (sum 1 5))))
+					; (inc (inc (inc (inc (sum
+					; (dec 1 ) 5)))))
+					; (inc (inc (inc (inc
+					; (sum 0 5)))))
+					; (inc (inc (inc (inc 5))))
+					; (inc (inc (inc 6)))
+					; (inc (inc 7))
+					; (inc 8)
+					; Value: 9
+
+					; this evaluation creates
+					; a recursive process as
+					; we need to maintain how many
+					; call to inc has been done
 
 
-; note that in both cases the procedure is recursive
-; as it calls itself from within. Yet the nature of the process varies.
-; Because a procedure is a syntactic model and process is an evolution model (execution)
+(define (sum_i a b)
+  (if (= a 0) b
+      (sum_i (dec a) (inc b))))
+
+(sum_i 4 5)				; (sum 4 5)
+					; (sum_i (dec 4) (inc 5))
+					; (sum_i 3 6)
+					; (sum_i (dec 3) (inc 6))
+					; (sum_i 2 7)
+					; (sum_i (dec 2) (inc 7))
+					; (sum_i 1 8)
+					; (sum_i (dec 1) (inc 8))
+					; (sum_i 0 9)
+					; Value: 9
+					; this evluation creates
+					; an iterative process as we
+					; only keep track of
+					; the parameters to sum_i
+
+
