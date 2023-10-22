dcbc12514f74d88293bd7fdbd413a1947f6dd328
amine177 <amri.amine@protonmail.com>
2023-10-22 14:54:52 +0200

    Added better comments

--- ch1/exe-1.10.scm
+++ ch1/exe-1.10.scm
@@ -24,8 +24,10 @@
 					; 2^16
 
 
-(define (f n) (A 0 n)) ; f x -> 2x
-(define (g n) (A 1 n)) ; g x -> 2^x
-(define (h n) (A 2 n)) ; h x -> 2^2^2..^2 , x times
-; (A 2 n) -> (A 1 (A 1 .... (A 2 (- n 1))))
-;  -> 2^(A 1 ..... (A 2 (- n 1)) -> 2^2^(.... (A 2 (- n 1 ) ))
+(define (f n) (A 0 n))			; f n -> 2n
+(define (g n) (A 1 n))			; g n -> 2^n
+(define (h n) (A 2 n))			; h n -> (A 1 (A 2 (- n 1)))
+					; -> 2^(A 2 (- n 1))
+					; -> 2^2^(A 2 (- n 2))
+					; -> 2^2^..^2 n times
+					; -> tetration
