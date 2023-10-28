5d47578404af3b8518eb68f2ed49fdd9fb79ff86
amine177 <amri.amine@protonmail.com>
2023-10-28 20:53:41 +0200

    Modified iterative exponentiation and added fast exp

--- ch1/exponentiation.scm
+++ ch1/exponentiation.scm
@@ -1,19 +1,28 @@
-(define (exp b n)
+(define (e b n)
   (if (= n 0)
       1
-      (* b (exp b (- n 1)))))
+      (* b (e  b (- n 1)))))
 
-(exp 2 2)
-(exp 4 2)
-(exp 5 3)
+(e 2 10)
 
-(define (exp-t b res n)
+
+(define (e-iter b n)
+  (define (e-iter-internal total x p)
+    (if (= p 0)
+	total
+	(e-iter-internal (* total x) x (- p 1))))
+  (e-iter-internal 1 b n))
+
+(e-iter 2 10)
+
+(define (even x)
+  (= (remainder x 2)  0))
+
+(define (fast-exp b n)
   (if (= n 0)
-      res
-      (exp-t b (* res b) (- n 1))))
+      1
+      (if (even n)
+	  (* (fast-exp b (/ n 2)) (fast-exp b (/ n 2)))
+	   (* b (fast-exp b (- n 1))))))
 
-(define (exp-it b n)
-  (exp-t b 1 n))
-
-(exp-it 4 2)
-  
+(fast-exp 2 10)
