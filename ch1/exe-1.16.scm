69a79f50c2c47dedfe61f79bc93e48098e1af5d9
amine177 <amri.amine@protonmail.com>
2023-10-29 11:15:30 +0100

    corrected mistakes

--- ch1/exe-1.16.scm
+++ ch1/exe-1.16.scm
@@ -1,16 +1,19 @@
-(define (fast-exp b n)
+ 
+(define (even x)
+  (= (remainder x 2)  0))
 
-  (define (fast-exp-iter b acc n)
-    (display n)
-    (display "\n")
-    (cond ((= n 0) 1)
-	 ((= n 1) acc)
-	 ((even n)
-	  (fast-exp-iter b (* acc acc) (/ n 2)))
-	 (else (fast-exp-iter b (* acc b) (- n 1)))))
-  (fast-exp-iter b b n))
 
-(define (even n)
-  (= (remainder n 2) 0))
+(define (fast-exponentiation-iterative b n)
+  (define (fa-e-iter b acc_even acc_odd n)
+    (cond
+     ((= n 0)
+      acc_odd)
+     ((even n)
+      (fa-e-iter b (* acc_even acc_even) acc_odd (/ n 2)))
+     (else
+      (fa-e-iter b acc_even (* acc_odd acc_even)  (- n 1)))))
+  (trace fa-e-iter)
+  (fa-e-iter b b 1 n))
 
-(fast-exp 4 2)
+
+(fast-exponentiation-iterative 4 2)
