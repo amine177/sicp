97a228caace54695b96af39cd012af598d9191cb
amine177 <amri.amine@protonmail.com>
2023-11-08 04:15:53 +0100

    added explanation

--- ch1/let.scm
+++ ch1/let.scm
@@ -1,9 +1,10 @@
 (define x 4)
 (let ((x 3)
-      (y (+ x 2))) ; y <- 4 + 2
-  (newline)
+      (y (+ x 2))) ; y <- 4 + 2 . The x here is the one from the 
+  (newline)        ; external scope
   (display x)
   (newline)
   (display y)
   (newline)
-  (display (+ x y))) ; y: 6, x: 3
+  (display (+ x y))) ; y: 6, x: 3. The x here is from the interal
+                     ; scope of the let expression (3)
