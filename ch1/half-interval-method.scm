4bdbb5564dcdbdb48eba5439dda9e4ce187dd7ef
amine177 <amri.amine@protonmail.com>
2023-11-08 06:13:14 +0100

    added check against non convergeant a & b values

--- ch1/half-interval-method.scm
+++ ch1/half-interval-method.scm
@@ -6,6 +6,16 @@
 		((< mid-image 0) (find-zero f mid b epsilon))
 		(else mid))))))
 
+(define (half-interval f a b epsilon)
+  (let ((a-image (f a))
+	(b-image (f b)))
+    (cond ((and (> a-image 0) (< b-image 0))
+	   (find-zero f b a epsilon))
+	  ((and (< a-image 0) (> b-image 0))
+	   (find-zero f a b epsilon))
+	  (else
+	   (error "(f " 'a' ")=" 'a-image' ", (f " b ")=" 'b-image'
+		  "are not of opposite signs")))))
 	       
 	       
 
@@ -15,4 +25,4 @@
 (define (square-minus-9 x)
   (- (* x x) 9))
 
-(find-zero square-minus-9 2 5 0.00000000000000001)
+(half-interval square-minus-9 2 2.5 0.00000000000000001)
