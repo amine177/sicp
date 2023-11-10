00519914974576d96bf6685e974ad4406166b79e
amine177 <amri.amine@protonmail.com>
2023-11-10 03:42:38 +0100

    exercise 1.35

---
+++ ch1/exe-1.35.scm
@@ -0,0 +1,15 @@
+(define (fixed-point f x epsilon)
+  (let ((current (f x))
+	(next (f (f x))))
+  (if (< (abs (- current next)) epsilon)
+      next
+      (fixed-point f current epsilon))))
+
+(define (golden-ratio x epsilon)
+  (fixed-point
+	 (lambda (x) (+ 1 (/ 1 x)))
+	 x
+	 epsilon))
+
+
+(golden-ratio 1.0 0.0001)
