c4779f0d2c80e708292a61dd59603046bc923dd1
amine177 <amri.amine@protonmail.com>
2023-11-10 04:26:37 +0100

    a diff procedure for dampening

--- ch1/exe-1.35.scm
+++ ch1/exe-1.35.scm
@@ -7,8 +7,13 @@
 
 (define (golden-ratio x epsilon)
   (fixed-point
-;   (lambda (x) (+ 1 (/ 1 x)))
    (lambda (y) (* 0.5 (+ y (+ 1 (/ 1 y))))) ; with dampening
+	 x
+	 epsilon))
+
+(define (golden-ratio x epsilon)
+  (fixed-point
+   (lambda (x) (+ 1 (/ 1 x)))
 	 x
 	 epsilon))
 
