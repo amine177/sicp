2a712c2d9a771cd0b31ca23ec90848025d2ebeb9
amine177 <amri.amine@protonmail.com>
2023-11-11 19:19:26 +0100

    added comment about newton to the book form relation

--- ch1/sqrt.scm
+++ ch1/sqrt.scm
@@ -5,6 +5,8 @@
 (define (calculate_next x_n x)		; x_n+1 = x_n - f(x_n) / f'(x_n)
 					; f(x) = x^2 - y , solve for f(x) = 0
  					;   x_n+1 = x_n - (x_n^2 - 2) / 2*(x_n)
+					; x_n+1 = x_n + (y/2x_n)
+  
 
   (- x_n (/ (- (* x_n x_n) 2) (* 2 x_n))))
 (define (abs a)
