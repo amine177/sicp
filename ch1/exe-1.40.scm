98e426d5b6b863fbb54abcac22c5c21e6d5f1718
amine177 <amri.amine@protonmail.com>
2023-11-11 19:19:59 +0100

    exe 1.40

---
+++ ch1/exe-1.40.scm
@@ -0,0 +1,42 @@
+(define (deriv g)
+  (let ((dx 0.000001))
+    (lambda (x) (/ (- (g (+ x dx))  (g x)) dx))))
+
+
+(define (square x)
+  (* x x))
+  
+
+
+(define (fixed-point f x epsilon)
+  (let ((current (f x))
+	(next (f (f x))))
+  (if (< (abs (- current next)) epsilon)
+      next
+      (fixed-point f current epsilon))))
+
+(define (newton-method g initial-guess)
+  (fixed-point
+   (lambda (x) (- x (/ (g x) ((deriv g) x)))) initial-guess 0.0001))
+
+
+(define (sqrt x)
+  (newton-method
+   (lambda (y) (- (square y) x)) 1.0))
+
+(sqrt 2)
+
+(define (cube x)
+  (* x x x))
+
+
+(define (cubic a b c)
+  (lambda (x) (+ (cube x) (* a (square x)) (* b x) c)))
+
+(define (solve-cubic a b c)
+  (newton-method
+   (cubic a b c) 1.0))
+
+
+(solve-cubic 1 100 1)
+
