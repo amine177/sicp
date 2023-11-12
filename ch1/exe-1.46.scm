e9493691e849822696df65244535dd2d2227a8f6
amine177 <amri.amine@protonmail.com>
2023-11-12 09:08:30 +0100

    exe 1.45

---
+++ ch1/exe-1.46.scm
@@ -0,0 +1,33 @@
+(define (iterative-improve predicate improve)
+  (define (iter guess)
+    (if (predicate guess (improve guess))
+	(improve guess)
+	(iter (improve guess))))
+  (lambda (x)
+    (trace iter)
+    (iter x)))
+
+
+
+
+
+
+(define (sqrt x)
+  ((iterative-improve  (lambda (a b) (< (abs (- a b)) 0.001))
+		       (lambda (guess) (average  guess (/ x guess))))
+		       1.0))
+
+
+
+
+(define (fixed-point f x epsilon)
+  (let ((current (f x))
+	(next (f (f x))))
+  (if (< (abs (- current next)) epsilon)
+      next
+      (fixed-point f current epsilon))))
+
+(define (fixed-point f x epsilon)
+  ((iterative-improve
+    (lambda (a b) (< (abs (- a b)) epsilon))
+    (lambda (guess) (f guess))) x))
