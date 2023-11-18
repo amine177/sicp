cb276771f6d2f42658c7af7fa9ec135b28b299b8
amine177 <amri.amine@protonmail.com>
2023-11-18 06:29:01 +0100

    exercise 2.7

---
+++ ch2/exe-2.7.scm
@@ -0,0 +1,26 @@
+(define (add-interval interval1 interval2)
+  (make-interval (+ (lower-bound interval1) (lower-bound interval2))
+		 (+ (upper-bound interval1) (upper-bound interval2))))
+
+(define (mul-interval x y)
+  (let ((ll (* (lower-bound x) (lower-bound y)))
+	(lu (* (lower-bound x) (upper-bound y)))
+	(ul (* (upper-bound x) (lower-bound y)))
+	(uu (* (upper-bound x) (upper-bound y))))
+    (make-interval (min ll lu ul uu)
+		   (max ll lu ul uu))))
+
+
+(define (div-interval x y)
+  (mul-interval x
+	       (make-interval (/ 1.0 (upper-bound y))
+			      (/ 1.0 (lower-bound y)))))
+
+(define (make-interval a b)
+  (cons a b))
+
+(define (upper-bound x)
+  (max (car x) (cdr x)))
+
+(define (lower-bound x)
+  (min (car x) (cdr x)))
