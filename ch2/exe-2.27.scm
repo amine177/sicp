d5fcc9606734c8e72299944edc544c16bc357e53
amine177 <amri.amine@protonmail.com>
2023-12-30 04:49:38 +0100

    added a method using map

--- ch2/exe-2.27.scm
+++ ch2/exe-2.27.scm
@@ -5,5 +5,18 @@
 		 (list (car lst))))
 	(else
 	 (append (deep-reverse (cdr lst))
-		(list (deep-reverse (car lst))))))))
+		(list (deep-reverse (car lst)))))))
 (deep-reverse (list 3 (list 1 (list 5 6) 3) 4))
+
+(define (reverse lst)
+  (cond ((null? lst) '())
+	(else
+	 (append (reverse (cdr lst))
+		 (list (car lst))))))
+
+(define (elegant-deep-reverse lst)
+  (cond ((pair? lst)
+	 (reverse (map elegant-deep-reverse lst)))
+	(else
+	 lst)))
+(elegant-deep-reverse (list 3 (list 1 (list 5 6) 3) 4))
