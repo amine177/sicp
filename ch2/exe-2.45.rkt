f80320790442d02ad33e13317efd9fd2dc7ec540
amine177 <amri.amine@protonmail.com>
2024-01-18 08:07:35 +0100

    exercise 2.45

---
+++ ch2/exe-2.45.rkt
@@ -0,0 +1,87 @@
+#lang sicp
+(#%require sicp-pict)
+
+(define wave
+  (segments->painter (list (make-segment
+                            (make-vect 0.40 1.00)
+                            (make-vect 0.35 0.95)) ; head left top line
+                           (make-segment
+                            (make-vect 0.45 1.00)
+                            (make-vect 0.50 0.95)) ; head left bottom line
+                           (make-segment
+                            (make-vect 0.35 0.95)
+                            (make-vect 0.4 0.90)) ; head right top line
+                           (make-segment
+                            (make-vect 0.50 0.95)
+                            (make-vect 0.45 0.90)) ; head right bottom line
+                           (make-segment
+                            (make-vect 0.40 0.90)
+                            (make-vect 0.30 0.905)) ; arm shoulder left
+                           (make-segment
+                            (make-vect 0.30 0.905)
+                            (make-vect 0.1 0.85)) ; arm arm left
+                           (make-segment
+                            (make-vect 0.1 0.85)
+                            (make-vect 0.00 0.80)) ; arm left hand
+                           (make-segment
+                            (make-vect 0.45 0.90)
+                            (make-vect 0.55 0.90))
+                           (make-segment
+                            (make-vect 0.55 0.90)
+                            (make-vect 0.75 0.845))
+                           (make-segment
+                            (make-vect 0.75 0.845)
+                            (make-vect 1 0.90))
+                           (make-segment
+                            (make-vect 0.75 0.800)
+                            (make-vect 1 0.865))
+                           (make-segment
+                            (make-vect 0.5  0.845)
+                            (make-vect 0.75 0.800))
+                           (make-segment
+                            (make-vect 0.5  0.845)
+                            (make-vect 0.5 0.7))
+                           (make-segment
+                            (make-vect 0.5 0.7)
+                            (make-vect 0.6 0.5))
+                           (make-segment
+                            (make-vect 0.6 0.5)
+                            (make-vect 0.5 0))
+                           (make-segment
+                            (make-vect 0.4 0)
+                            (make-vect 0.5 0.5))
+                           (make-segment
+                            (make-vect 0.5 0.5)
+                            (make-vect 0.4 0.7))
+                           (make-segment
+                            (make-vect 0.4 0.7)
+                            (make-vect 0.3 0.5))
+                           (make-segment
+                            (make-vect 0.3 0.7)
+                            (make-vect 0.2 0.5))
+                           (make-segment
+                            (make-vect 0.3 0.5)
+                            (make-vect 0.4 0))
+                           (make-segment
+                            (make-vect 0.2 0.5)
+                            (make-vect 0.3 0))
+                           (make-segment
+                            (make-vect 0.3 0.7)
+                            (make-vect 0.3 0.845))
+                           (make-segment
+                            (make-vect 0.3 0.845)
+                            (make-vect 0.1 0.80))
+                           (make-segment
+                            (make-vect 0.1 0.8)
+                            (make-vect 0 0.75)))))
+
+(define (split left right)
+  (lambda (painter n)
+    (if (= n 0)
+        painter
+        (let ((rest ((split left right) painter (- n 1))))
+          (right painter (left rest rest))))))
+
+(define right-split (split beside below))
+(define up-split (split below beside))
+
