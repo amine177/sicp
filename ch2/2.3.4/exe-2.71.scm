b30c14d58d8d6c3c1b5ce8320a1cfaa89702a9c6
amine177 <amri.amine@protonmail.com>
2024-02-29 05:43:02 +0100

    exercise 2.71

---
+++ ch2/2.3.4/exe-2.71.scm
@@ -0,0 +1,48 @@
+; n = 5
+; number-bits-most-frequent : 1
+; number-bits-least-frequent : 4
+
+'((S1 1) (S2 2) (S3 4) (S4 16) (S5 64))
+'(((((leaf S1 1)
+     (leaf S2 2)
+     (S1 S2)
+     3)
+    (leaf S3 4)
+    (S1 S2 S3)
+    7)
+   (leaf S4 16)
+   (S1 S2 S3 S4)
+   23)
+  (leaf S8 64)
+  (S1 S2 S3 S4 S5)
+  87)
+ 
+					; n = 10
+					; number-bits-most-frequent : 1
+; number-bits-least-frequent : 8
+
+'((S1 1) (S2 2) (S3 4) (S4 16)
+  (S5 64) (S6 128) (S7 512)
+  (S8 1024) (S9 2048) (S10 4056))
+
+'(((((((((leaf S1 1)
+     (leaf S2 2)
+     (S1 S2)
+     3)
+    (leaf S3 4)
+    (S1 S2 S3)
+    7)
+   (leaf S4 16)
+   (S1 S2 S3 S4)
+   23)
+  (leaf S8 64)
+  (S1 S2 S3 S4 S5)
+  87)
+  (leaf S6 215)
+  (S1 S2 S3 S4 S5 S6))
+  (leaf S7 727)
+  (S1 S2 S3 S4 S5 S6 S7))
+  (leaf S8 1751)
+  (S1 S2 S3 S4 S5 S6 S7 S8))
+  (leaf S9 3799)
+  (S1 S2 S3 S4 S5 S6 S7 S8 S9))
