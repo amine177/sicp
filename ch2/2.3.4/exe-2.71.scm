f13edd82c04df112b611254c9dee9fcbf22c20b3
amine177 <amri.amine@protonmail.com>
2024-03-01 08:27:42 +0100

    correction

--- ch2/2.3.4/exe-2.71.scm
+++ ch2/2.3.4/exe-2.71.scm
@@ -17,15 +17,15 @@
   (S1 S2 S3 S4 S5)
   87)
  
-					; n = 10
-					; number-bits-most-frequent : 1
-; number-bits-least-frequent : 8
+; n = 10
+; number-bits-most-frequent : 1
+; number-bits-least-frequent : 10 
 
 '((S1 1) (S2 2) (S3 4) (S4 16)
   (S5 64) (S6 128) (S7 512)
   (S8 1024) (S9 2048) (S10 4056))
 
-'(((((((((leaf S1 1)
+'((((((((((leaf S1 1)
      (leaf S2 2)
      (S1 S2)
      3)
@@ -38,11 +38,12 @@
   (leaf S8 64)
   (S1 S2 S3 S4 S5)
   87)
-  (leaf S6 215)
-  (S1 S2 S3 S4 S5 S6))
-  (leaf S7 727)
-  (S1 S2 S3 S4 S5 S6 S7))
-  (leaf S8 1751)
-  (S1 S2 S3 S4 S5 S6 S7 S8))
-  (leaf S9 3799)
-  (S1 S2 S3 S4 S5 S6 S7 S8 S9))
+  (leaf S6 128)
+  (S1 S2 S3 S4 S5 S6) 215)
+  (leaf S7 512)
+  (S1 S2 S3 S4 S5 S6 S7) 727)
+  (leaf S8 1024)
+  (S1 S2 S3 S4 S5 S6 S7 S8) 1751)
+  (leaf S9 2048)
+  (S1 S2 S3 S4 S5 S6 S7 S8 S9) 3799)
+  (leaf S10 4056) 7855)
