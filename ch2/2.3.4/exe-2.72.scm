d8217448b638a261fb6a00661118f2565f20b573
amine177 <amri.amine@protonmail.com>
2024-03-04 07:41:46 +0100

    added more context

--- ch2/2.3.4/exe-2.72.scm
+++ ch2/2.3.4/exe-2.72.scm
@@ -6,3 +6,11 @@
 ; O(encode) = sum(i: 1->n, N_Si * O(log(i))) close to O(n log(n))
 ; In the case of an unblanced tree
 ; O(encode) close to O(n^2)
+; The optimal case is for any symbole Si of length li
+; to satisfy : li = -log_2(1/weight_Si)
+; then weight_Si = 1/2^i
+; this decreasing nature of the symbol
+; weights should be satisfied even with
+; natural number, therfore the weights of 2, 2^2, ... , 2^n
+; converge in their order of growth to the worst case and that is
+; O(n^2)
